3eeb01e855e2b57be0db2578bd735c286871118a
wjl_windows <wlong108@iCloud.com>
2021-05-23 16:50:37 +0800

    windows引入ffmpeg

---
+++ qt_first/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_helloQT_clicked()
+{
+    qDebug() << "void MainWindow::on_helloQT_clicked()";
+}
